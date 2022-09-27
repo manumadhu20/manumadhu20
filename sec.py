2c923f8f9bbc25c1c9751563c3bfb926c2783138
manumadhu20 <110897057+manumadhu20@users.noreply.github.com>
2022-09-27 10:48:02 +0530

    Add files via upload

--- sec.py
+++ sec.py
@@ -1,7 +1,32 @@
-num=[4,2,1,3]
-total=0
-for i in range(len(num)):
-    if(i%2)!=0:
-        print(num[i])
-        total=total+num[i]
-        print("sum of numbers",total)
+lst = []
+  
+n = int(input("Enter number of elements : "))
+  
+for i in range(0, n):
+    e = int(input())
+    lst.append(e) 
+print(lst)
+
+max_1=max(lst)
+
+
+
+if n==3:
+  print("the possible outcomes to rob is",max_1)
+else:
+  print("")  
+
+
+if n==4:
+   if lst.index(max_1)==0:
+     print("the possible outcomes are",max_1+lst[2])
+   elif lst.index(max_1)==1:
+    print("the possible outcomes are",max_1+lst[3])
+   elif lst.index(max_1)==2:
+    print("the possible outcomes are",max_1+lst[0])
+   elif lst.index(max_1)==3:
+    print("the possible outcomes are",max_1+lst[1])
+   else:
+    print("no possible outcomes")
+    
+    

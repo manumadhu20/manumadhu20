d06ab4a655c438b3e1d7eed845737b83a7309d45
manumadhu20 <110897057+manumadhu20@users.noreply.github.com>
2022-10-12 11:37:05 +0530

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,27 @@
+--create database musaliar;
+use musaliar;
+--create table mcet(
+--name varchar(25),
+--age int,
+--branch varchar(10),
+--admissionno int,
+--universityid varchar(25));
+select * from mcet;
+--insert into mcet(
+--name,age,branch,admissionno,universityid)
+--values(
+--'jithu',22,'cse',18263,'mck18cs');
+--create table semesters(
+--s1 int,subname varchar(25),cgpa int,sgpa int, grade varchar(5));
+select * from semesters;
+--insert into semesters(
+--s1,subname,cgpa,sgpa,grade)
+--values(
+--6,'calculus',10,10,'o');
+--create table departmentstaffs(
+--staffname varchar(25),age int, experience varchar(10));
+--insert into departmentstaffs(
+--staffname,age,experience)
+--values(
+--'deepathomas',40,'21 years');
+select * from departmentstaffs;
